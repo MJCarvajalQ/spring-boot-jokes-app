4b7bf4c85f1562348b575ea331b5a1f68422c051
MJCarvajalQ <majo_c_q@hotmail.com>
2024-12-12 15:35:26 +0700

    Initial Project Add

---
+++ src/main/java/guru/springframework/spring_boot_jokes_app/SpringBootJokesAppApplication.java
@@ -0,0 +1,13 @@
+package guru.springframework.spring_boot_jokes_app;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringBootJokesAppApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringBootJokesAppApplication.class, args);
+	}
+
+}
---
+++ src/test/java/guru/springframework/spring_boot_jokes_app/SpringBootJokesAppApplicationTests.java
@@ -0,0 +1,13 @@
+package guru.springframework.spring_boot_jokes_app;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringBootJokesAppApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
